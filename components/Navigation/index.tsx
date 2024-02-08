6de011ec71261555afac4ca2684a081adea18d3a
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-02-09 00:27:57 +0630

    feat: hero and navigation

---
+++ components/Navigation/index.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+function Navigation() {
+  return (
+    <header className="bg-white fixed top-12 left-0 right-0 rounded-full p-3 max-w-[80%] mx-auto shadow flex items-center space-x-8">
+      <div className="flex items-center space-x-5">
+        <div className="w-12 h-12 rounded-full overflow-hidden">
+          <Image
+            src="/512.png"
+            alt="Avatar"
+            width={300}
+            height={300}
+            className="object-cover"
+          />
+        </div>
+        <h3 className="font-bold">HWP.SWE</h3>
+      </div>
+      <div className="w-[1px] h-5 bg-gray-dark" />
+      <nav className="flex justify-between items-center w-full">
+        <ul className="flex items-center space-x-8">
+          <li className="text-sm">About</li>
+          <li className="text-sm">Pricing</li>
+          <li className="text-sm">Projects</li>
+          <li className="text-sm">Blogs</li>
+        </ul>
+        <button className="bg-primary text-black rounded-full px-7 py-3">
+          Contact
+        </button>
+      </nav>
+    </header>
+  );
+}
+
+export default Navigation;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,19 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    extend: {},
+    colors: {
+      primary: {
+        DEFAULT: "#FEC857",
+        dark: "#feb724",
+        darker: "#EEA101",
+      },
+      white: "#ffffff",
+      black: "#010101",
+      gray: {
+        lightest: "#f7f7f7",
+        dark: "#f7f7f8",
+        DEFAULT: "#ebebeb",
       },
     },
   },
